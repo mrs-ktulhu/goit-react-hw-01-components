133a527bbe42497178b5cddef297c9d55881386f
Mamba <dgessika.lov@gmail.com>
2023-05-16 22:53:17 +0300

    add components to App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,23 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import user from '../user.json';
+import data from '../data.json';
+import friends from '../friends.json';
+import transactions from '../transactions.json';
+import { Profile } from './Profile';
+import { Statistics } from './Statistics';
+import { FriendsList } from './FriendList';
+import { TransactionHistory } from './TransactionHistory';
+
+export const App = () => (
+  <>
+    <Profile
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+    />
+    <Statistics data={data} />
+    <FriendsList friends={friends} />
+    <TransactionHistory transactions={transactions}/>
+  </>
+);
