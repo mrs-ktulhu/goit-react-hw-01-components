f6631963ba607beb2537dca728269c5150ff5d21
Mamba <dgessika.lov@gmail.com>
2023-05-16 22:43:54 +0300

    user profile component

---
+++ src/components/Profile.jsx
@@ -0,0 +1,43 @@
+import PropTypes from "prop-types";
+
+export const Profile = props => (
+  <div className="profile">
+    <div className="description">
+      <img
+        className="avatar"
+        src={props.avatar}
+        alt={props.username}
+      />
+      <p className="name">{props.username}</p>
+      <p className="tag">@{props.tag}</p>
+      <p className="location">{props.location}</p>
+    </div>
+    
+    <ul className="stats">
+      <li>
+        <span className="label">Followers</span>
+        <span className="quantity">{props.stats.followers}</span>
+      </li>
+      <li>
+        <span className="label">Views</span>
+        <span className="quantity">{props.stats.views}</span>
+      </li>
+      <li>
+        <span className="label">Likes</span>
+        <span className="quantity">{props.stats.likes}</span>
+      </li>
+    </ul>
+  </div>
+)
+
+Profile.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  stats: PropTypes.shape({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+  })
+};
