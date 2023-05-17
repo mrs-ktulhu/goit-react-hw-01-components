dc71e2f7b45898d7e73d9918f702f5e7d1cc62e6
Mamba <dgessika.lov@gmail.com>
2023-05-17 23:31:35 +0300

    add styles for profile

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,32 +1,33 @@
 import PropTypes from "prop-types";
+import './styles/profile.css';
 
 export const Profile = props => (
   <div className="profile">
-    <div className="description">
-      <img
-        className="avatar"
-        src={props.avatar}
-        alt={props.username}
-      />
-      <p className="name">{props.username}</p>
-      <p className="tag">@{props.tag}</p>
-      <p className="location">{props.location}</p>
-    </div>
-    
-    <ul className="stats">
-      <li>
-        <span className="label">Followers</span>
-        <span className="quantity">{props.stats.followers}</span>
-      </li>
-      <li>
-        <span className="label">Views</span>
-        <span className="quantity">{props.stats.views}</span>
-      </li>
-      <li>
-        <span className="label">Likes</span>
-        <span className="quantity">{props.stats.likes}</span>
-      </li>
-    </ul>
+      <div className="description">
+        <img
+          className="avatar"
+          src={props.avatar}
+          alt={props.username}
+        />
+        <p className="name">{props.username}</p>
+        <p className="tag">@{props.tag}</p>
+        <p className="location">{props.location}</p>
+      </div>
+      
+      <ul className="stats">
+        <li>
+          <span className="label">Followers</span>
+          <span className="quantity">{props.stats.followers}</span>
+        </li>
+        <li>
+          <span className="label">Views</span>
+          <span className="quantity">{props.stats.views}</span>
+        </li>
+        <li>
+          <span className="label">Likes</span>
+          <span className="quantity">{props.stats.likes}</span>
+        </li>
+      </ul>
   </div>
 )
 
